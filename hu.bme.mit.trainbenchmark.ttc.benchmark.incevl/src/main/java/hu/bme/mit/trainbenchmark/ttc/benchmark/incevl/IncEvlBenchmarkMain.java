8c3eae398a881889b3d41485acc9921097eb30b1
Jonathan Co <jonathanco1991@gmail.com>
2016-01-19 12:48:41 +0000

    Added stub project for Incremental Evl benchmark

---
+++ hu.bme.mit.trainbenchmark.ttc.benchmark.incevl/src/main/java/hu/bme/mit/trainbenchmark/ttc/benchmark/incevl/IncEvlBenchmarkMain.java
@@ -0,0 +1,9 @@
+package hu.bme.mit.trainbenchmark.ttc.benchmark.incevl;
+
+public class IncEvlBenchmarkMain {
+
+	public static void main(String[] args) {
+		System.out.println("not implemented");
+	}
+
+}
