16434a4bc43a8eaa861321e0cc55699d09ed134a
Jonathan Co <jonathanco1991@gmail.com>
2016-01-19 12:49:05 +0000

    Added stub project for Standard Evl Benchmark

---
+++ hu.bme.mit.trainbenchmark.ttc.benchmark.evl/src/main/java/hu/bme/mit/trainbenchmark/ttc/benchmark/evl/EvlBenchmarkMain.java
@@ -0,0 +1,9 @@
+package hu.bme.mit.trainbenchmark.ttc.benchmark.evl;
+
+public class EvlBenchmarkMain {
+
+	public static void main(String[] args) {
+		System.out.println("not implemented");
+	}
+
+}
